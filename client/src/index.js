5f5c2fb97596052b9c1fa91f3a1d91e5e6291f78
Alicia Silverio <aliciaalicia2455@gmail.com>
2022-07-07 04:55:10 -0400

    chat css and join css

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
-import * as ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.createRoot(<App />, document.querySelector("#root")).render(<React.StrictMode></React.StrictMode>);
+
+
+
+// ReactDOM.render(<App />, document.querySelector('#root'));
+
+// import { createRoot } from 'react-dom/client';
+// const container = document.getElementById('app');
+// const root = createRoot(container); // createRoot(container!) if you use TypeScript
+// root.render(<App tab="home" />);
