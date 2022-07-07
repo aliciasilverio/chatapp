1878319a6824df76d1bb80c4274ded0a781393ef
Alicia Silverio <aliciaalicia2455@gmail.com>
2022-07-07 09:11:04 -0400

    july 7 910am

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { createRoutesFromChildren, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Join.css';
 
 const Join = () => {
--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,27 @@ import * as ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-ReactDOM.createRoot(<App />, document.querySelector("#root")).render(<React.StrictMode></React.StrictMode>);
 
+// const container = document.getElementById('root');
+// const root = ReactDOM.createRoot(container); 
+// root.render(<App />);
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>
+// );
+
+
+// ReactDOM.createRoot(<App />, document.querySelector("root")).render(<React.StrictMode></React.StrictMode>);
 
 
 // ReactDOM.render(<App />, document.querySelector('#root'));
 
+
 // import { createRoot } from 'react-dom/client';
 // const container = document.getElementById('app');
-// const root = createRoot(container); // createRoot(container!) if you use TypeScript
+// const root = createRoot(container); 
 // root.render(<App tab="home" />);
